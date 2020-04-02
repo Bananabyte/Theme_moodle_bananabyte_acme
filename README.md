b2199be8e8791328ac91016d9ca3b63293c7d56c
Marco Valadares <bananabyte@gmail.com>
2020-04-02 00:15:18 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Theme_moodle_bananabyte_acme
+This theme is fluid 3 columns with a dark design.
---
+++ version.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * This is the version.php file for acmebanana theme.
+ */
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version   = 2013010101;
+$plugin->component = 'theme_acmebanana';
+$plugin->requires  = 2010112400;
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release = 1.03;
+
+?>
